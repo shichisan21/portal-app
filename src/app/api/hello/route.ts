66817844c8b7e3feec839a8578000ea0ae4d171e
shichisan21 <photograph.by@gmail.com>
2024-05-04 01:11:50 +0900

    コメント削除

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -6,15 +6,5 @@ type ResponseData = {
 
 export async function GET(request: NextApiRequest) {
   console.log(request.body);
-  // const body =await request.json()
-
-  // console.log("POST request", body.name)
   return new Response(JSON.stringify({ message: "Hello World" }));
 }
-
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<ResponseData>
-// ) {
-//   res.status(200).json({ message: "Hello from Next.js!" });
-// }
